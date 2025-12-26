e350a8f98f7a775f9f1118dc9b32a5d26a3627e1
Aubregine <aubregines@gmail.com>
2025-12-26 17:33:21 +0100

    Small optimization

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use std::fmt::{Debug, Formatter};
 use std::ops::{Add, Mul};
-use std::sync::{Arc, Mutex};
 use std::sync::mpsc::Receiver;
 use macroquad::color::hsl_to_rgb;
 use macroquad::prelude::*;
@@ -8,6 +7,7 @@ use rayon::prelude::*;
 use Divergence::*;
 
 const MAX_ITER: usize = 256;
+const ITER_MULTIPLIER: f32 = 1.2;
 const MAX_RADIUS: f64 = 2.0;
 const MOVE_AMOUNT: f64 = 0.1;
 
@@ -15,7 +15,6 @@ const MOVE_AMOUNT: f64 = 0.1;
 /// DONE: parallelize the computation of the divergence
 /// DONE: save the result of the computation to an image
 /// DONE: compute the color of the background based on the result of the iteration
-/// TODO: make zoom framerate independent
 #[macroquad::main("Mandelbrot Viewer")]
 async fn main() {
     let mut max_iter = MAX_ITER;
@@ -30,9 +29,7 @@ async fn main() {
     let image = Image::gen_image_color(image_x, image_y, WHITE);
     let texture = Texture2D::from_image(&image);
 
-    let image = Arc::new(Mutex::new(image));
     let mut rx = parallel_draw_image(
-        image.clone(),
         &canva_x,
         &canva_y,
         image_x as usize,
@@ -42,8 +39,7 @@ async fn main() {
 
     loop {
         clear_background(WHITE);
-        if let Ok(new_image) = rx.try_recv() {
-            let img = new_image.lock().unwrap();
+        if let Ok(img) = rx.try_recv() {
             texture.update(&img);
         }
 
@@ -51,28 +47,30 @@ async fn main() {
             break;
         }
 
-        if let Some(receiver) = handle_key_pressed(&mut max_iter, &mut center, &mut zoom, image_x, image_y, &image) {
+        if let Some(receiver) = handle_key_pressed(&mut max_iter, &mut center, &mut zoom, image_x, image_y) {
             rx = receiver;
         }
 
         draw_texture(&texture, 0.0, 0.0, WHITE);
         draw_rectangle(0.0, 0.0, 72.0, 24.0, WHITE);
-        draw_text(&max_iter.to_string(), 10.0, 20.0, 20.0, BLACK);
+        draw_text(&max_iter.to_string(), 10.0, 22.0, 20.0, BLACK);
+        draw_rectangle(0.0, 26.0, 72.0, 24.0, WHITE);
+        draw_text(&format!("{:.2e}x", 2.0/zoom), 10.0, 46.0, 20.0, BLACK);
         next_frame().await;
     }
 }
 
-fn handle_key_pressed(max_iter: &mut usize, center: &mut Complex, zoom: &mut f64, image_x: u16, image_y: u16, image: &Arc<Mutex<Image>>) -> Option<Receiver<Arc<Mutex<Image>>>> {
+fn handle_key_pressed(max_iter: &mut usize, center: &mut Complex, zoom: &mut f64, image_x: u16, image_y: u16) -> Option<Receiver<Image>> {
     let mut changed = false;
 
     if is_key_pressed(KeyCode::KpAdd) {
         *zoom /= 2.0;
-        *max_iter = (*max_iter as f32 * 1.1) as usize; // this is also an issue
+        *max_iter = (*max_iter as f32 * ITER_MULTIPLIER) as usize; // this is also an issue
         changed = true;
     }
     if is_key_pressed(KeyCode::KpSubtract) {
         *zoom *= 2.0;
-        *max_iter = (*max_iter as f32 / 1.1) as usize;
+        *max_iter = (*max_iter as f32 / ITER_MULTIPLIER) as usize;
         changed = true;
     }
     if is_key_pressed(KeyCode::Up) {
@@ -96,7 +94,6 @@ fn handle_key_pressed(max_iter: &mut usize, center: &mut Complex, zoom: &mut f64
         let canva_x = Range { start: center.re - *zoom, end: center.re + *zoom };
         let canva_y = Range { start: center.im - *zoom, end: center.im + *zoom };
         Some(parallel_draw_image(
-            image.clone(),
             &canva_x,
             &canva_y,
             image_x as usize,
@@ -109,21 +106,21 @@ fn handle_key_pressed(max_iter: &mut usize, center: &mut Complex, zoom: &mut f64
 
 
 fn parallel_draw_image(
-    image_arc: Arc<Mutex<Image>>,
     canva_x: &Range,
     canva_y: &Range,
     image_width: usize,
     image_height: usize,
     max_iter: usize,
-) -> Receiver<Arc<Mutex<Image>>> {
+) -> Receiver<Image> {
     let (tx, rx) = std::sync::mpsc::channel();
     let canva_x = canva_x.clone();
     let canva_y = canva_y.clone();
     let scale_x = canva_x.size() / (image_width as f64 - 1.0);
     let scale_y = canva_y.size() / (image_height as f64 - 1.0);
 
+
     std::thread::spawn(move || {
-        let mut image = image_arc.lock().unwrap();
+        let mut image = Image::gen_image_color(image_width as u16, image_height as u16, WHITE);
         let pixels = image.get_image_data_mut();
         pixels
             .par_chunks_mut(image_width)
@@ -148,15 +145,15 @@ fn parallel_draw_image(
                     // directly set the raw pixel data
                     *pixel = match div {
                         After(j) => {
-                            let val = (j * 255.0) as u8;
-                            [val, val, val, 255] // grey
+                            let val = j as u8;
+                            [val, val, val, 255] // gery
                         },
                         Never => [0, 0, 0, 255], // black
                     }
                 }
             });
 
-        let _ = tx.send(image_arc.clone());
+        let _ = tx.send(image);
     });
     rx
 }
@@ -175,7 +172,7 @@ impl Range {
 
 #[derive(Clone)]
 enum Divergence {
-    After(f32),
+    After(usize),
     Never,
 }
 
@@ -185,10 +182,11 @@ fn iterate(c: Complex, max_iter: usize, max_radius: f64) -> Divergence
     for i in 0..max_iter {
         z = z * z + c;
         if z.module_sqr() > max_radius * max_radius {
-            let delta = (0.5 * z.module_sqr().log2() / max_radius.log2()).log2() as f32;
-            let t = (i as f32 + 1.0 - delta) / max_iter as f32;
-            let shade = t.powf(0.6);
-            return After(shade.min(1.0));
+            // let delta = (0.5 * z.module_sqr().log2() / max_radius.log2()).log2() as f32;
+            // let t = (i as f32 + 1.0 - delta) / max_iter as f32;
+            // let shade = t.powf(0.6);
+            // return After(shade.min(1.0));
+            return After(i);
         }
     }
     Never
